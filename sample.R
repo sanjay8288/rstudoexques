1ffe1b3644381d91b08c3ec8b8802f9e999f46d8
sanjay8288 <psanjaykumar150@gmail.com>
2024-02-22 11:45:51 +0530

    Update sample.R
    vectors

--- sample.R
+++ sample.R
@@ -1,1 +1,11 @@
-#this is my first git hubcommit 
+numeric_vector <- c(1, 2, 3, 4, 5)
+character_vector <- c("apple", "orange", "banana")
+logical_vector <- c(TRUE, FALSE, TRUE)
+print(numeric_vector)
+print(character_vector)
+print(logical_vector)
+
+vec1 <- c(1, 2, 3)
+vec2 <- c(4, 5, 6)
+result_addition <- vec1 + vec2
+print(result_addition)
